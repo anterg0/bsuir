23aa04d4d8b8c9c2e868fb610b69717cc0f6d34a
Anton Shpakovsky <antonxll50@gmail.com>
2022-09-07 23:39:31 +0300

    Update ex8.cpp

--- lab1/ex8.cpp
+++ lab1/ex8.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cmath>
 
 int main() {
     int h1,min1,h2,min2,s1,s2,s3,h3,min3;
@@ -13,7 +12,7 @@ int main() {
         s3 = s2 - s1;
     }
     h3 = s3 / 3600;
-    min3 = fmod(s3, 3600);
+    min3 = s3 % 3600;
     min3 = min3 / 60;
     std::cout << (h3 < 10 ? "0" : "") << h3 << ":" << (min3 < 10 ? "0" : "") << min3;
     return 0;
