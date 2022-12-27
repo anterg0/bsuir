69975ff9bc2935e7c7b867be867523a1f9d36003
Anton Shpakovsky <antonxll50@gmail.com>
2022-12-27 21:09:51 +0300

    task1 changed and task2 started

---
+++ lab8/task1/bebra.hpp
@@ -0,0 +1,15 @@
+#ifndef bebra_hpp
+#define bebra_hpp
+
+#include <stdio.h>
+
+void func1();
+void func2();
+void func3();
+void func4();
+void func5();
+void func6();
+void func7();
+void func8();
+
+#endif
--- lab8/task2/main.cpp
+++ lab8/task2/main.cpp
@@ -1,14 +1,45 @@
-//
-//  main.cpp
-//  task2
-//
-//  Created by Антон Шпаковский on 26.12.2022.
-//
-
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+struct Book {
+    long long bookID;
+    struct {
+        std::string authorSurname;
+        std::string authorName;
+    } author;
+    std::string name;
+    int year;
+    std::string publisher;
+    int pageCount;
+};
+int ind = 0;
+Book *books = new Book[1000];
+
+std::fstream bebrochka();
+
+int main() {
+    while (ind < 3) {
+        Book book;
+        std::cout << "Введите регистрационный номер книги: ";
+        std::cin >> book.bookID;
+        std::cout << "Введите фамилию автора: ";
+        std::cin >> book.author.authorSurname;
+        std::cout << "Введите имя автора: ";
+        std::cin >> book.author.authorName;
+        std::cout << "Введите название книги: ";
+        std::cin >> book.name;
+        std::cout << "Введите год издания книги: ";
+        std::cin >> book.year;
+        std::cout << "Введите издателя книги: ";
+        std::cin >> book.publisher;
+        std::cout << "Введите кол-во страниц книги: ";
+        std::cin >> book.pageCount;
+        books[ind] = book;
+        ind++;
+        bebrochka.open();
+        bebrochka << books[0].bookID << " " << books[0].author.authorSurname << " " << books[0].author.authorName << " " << books[0].name << " " << books[0].year << " " << books[0].publisher << " " << books[0].pageCount << std::endl;
+        bebrochka.close();
+    }
+    
     return 0;
 }
