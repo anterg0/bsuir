9918c93ab9d33e973cc9a99a5d698a162c8c0585
Anton Shpakovsky <antonxll50@gmail.com>
2023-04-27 21:11:36 +0300

    i guess lab4 for C# is done?

--- C# PROGRAMMING/Lab4/Lab4/AirportTickets.cs
+++ C# PROGRAMMING/Lab4/Lab4/AirportTickets.cs
@@ -1,76 +1,107 @@
 namespace Lab4;
+public class AirportTicketContainer
+{
+    private static AirportTicketContainer instance = null;
+    private List<AirportTicket> tickets = new List<AirportTicket>();
+    private int SoldTickets = 0;
+    // public string AirportName { get; set; } 
+    // public int TicketCost { get; set; }
+    public int AvailableSeats { get; set; }
 
-class AirportTicket {
-    public string AirportName { get; set; }
-    public float TicketPrice { get; set; }
-    public int TotalSeats { get; set; }
-    public int SoldTickets { get; set; }
+    private AirportTicketContainer() {}
+    
+    public static AirportTicketContainer GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new AirportTicketContainer();
+        }
 
-    public AirportTicket(string airportName, float ticketPrice, int totalSeats, int soldTickets) {
-        AirportName = airportName;
-        TicketPrice = ticketPrice;
-        TotalSeats = totalSeats;
-        SoldTickets = soldTickets;
+        return instance;
     }
 
-    public float CalculateTotalSales() {
-        return SoldTickets * TicketPrice;
+    public void AddTicket(AirportTicket ticket)
+    {
+        if (SoldTickets < AvailableSeats)
+        {
+            tickets.Add(ticket);
+            SoldTickets++;
+        } else throw new ArgumentOutOfRangeException("AvailableSeats","Not enough available seats for that amount of tickets.");
+    }
+    
+    public void AddTicket(AirportTicket ticket, int amount)
+    {
+        if (SoldTickets + amount <= AvailableSeats)
+        {
+            for (int i = 0; i < amount; i++) tickets.Add(ticket);
+            SoldTickets += amount;
+        } else throw new ArgumentOutOfRangeException("AvailableSeats","Not enough available seats for that amount of tickets.");
     }
 
-    public void UpdateTicketPrice(float newPrice) {
-        TicketPrice = newPrice;
+    public void RemoveTicket(int index)
+    {
+        if (AvailableSeats != 0)
+        {
+            tickets.RemoveAt(index);
+            SoldTickets--;
+        }
+    }
+
+    public int TotalTicketsCost()
+    {
+        int sum = 0;
+        foreach (AirportTicket ticket in tickets)
+        {
+            sum += ticket.Cost;
+        }
+
+        return sum;
+    }
+    
+}
+
+public class AirportTicket
+{
+
+    private string _airportName;
+    private int _cost;
+    
+    public string AirportName
+    {
+        get { return _airportName; }
+        set { _airportName = value; }
+    }
+
+    public int Cost
+    {
+        get { return _cost; }
+        set { _cost = value; }
     }
 }
 
-class AirportTicketContainer {
-    private List<AirportTicket> airportTickets;
-
-    private static AirportTicketContainer instance = null;
-    private static readonly object padlock = new object();
-
-    AirportTicketContainer() {
-        airportTickets = new List<AirportTicket>();
+public class TicketInterface
+{
+    public void IncreaseCost(AirportTicket ticket, int amount)
+    {
+        ticket.Cost += amount;
     }
 
-    public static AirportTicketContainer Instance {
-        get {
-            lock (padlock) {
-                if (instance == null) {
-                    instance = new AirportTicketContainer();
-                }
-                return instance;
-            }
-        }
+    public int GetCost(AirportTicket ticket)
+    {
+        return ticket.Cost;
+    }
+    public void DecreaseCost(AirportTicket ticket, int amount)
+    {
+        ticket.Cost -= amount;
+    }
+    
+    public void ChangeAirportName(AirportTicket ticket, string name)
+    {
+        ticket.AirportName = name;
     }
 
-    public void AddTicket(AirportTicket airportTicket) {
-        airportTickets.Add(airportTicket);
-    }
-
-    public void RemoveTicket(AirportTicket airportTicket) {
-        airportTickets.Remove(airportTicket);
-    }
-
-    public List<AirportTicket> GetTickets() {
-        return airportTickets;
-    }
-
-    public AirportTicket GetTicketByName(string airportName) {
-        return airportTickets.Find(ticket => ticket.AirportName == airportName);
-    }
-
-    public AirportTicket GetTicketByIndex(int index) {
-        if (index >= 0 && index < airportTickets.Count) {
-            return airportTickets[index];
-        } else {
-            return null;
-        }
-    }
-
-    public void UpdateTicketPrice(string airportName, float newPrice) {
-        AirportTicket airportTicket = GetTicketByName(airportName);
-        if (airportTicket != null) {
-            airportTicket.UpdateTicketPrice(newPrice);
-        }
+    public string GetAirportName(AirportTicket ticket)
+    {
+        return ticket.AirportName;
     }
 }
--- C# PROGRAMMING/Lab4/Lab4/Program.cs
+++ C# PROGRAMMING/Lab4/Lab4/Program.cs
@@ -1,2 +1,23 @@
 ﻿using Lab4;
 
+Random rand = new Random();
+AirportTicketContainer container = AirportTicketContainer.GetInstance();
+AirportTicket ticket = new AirportTicket();
+TicketInterface inter = new TicketInterface();
+
+inter.IncreaseCost(ticket,1000);
+Console.WriteLine(inter.GetCost(ticket));
+inter.IncreaseCost(ticket, rand.Next(3001));
+inter.ChangeAirportName(ticket,"Belavia");
+
+container.AvailableSeats = 376;
+container.AddTicket(ticket, 375);
+
+Console.WriteLine(inter.GetCost(ticket));
+Console.WriteLine(container.TotalTicketsCost());
+
+AirportTicketContainer con2 = AirportTicketContainer.GetInstance();
+
+Console.WriteLine(con2.TotalTicketsCost());
+Console.WriteLine(container.TotalTicketsCost());
+
