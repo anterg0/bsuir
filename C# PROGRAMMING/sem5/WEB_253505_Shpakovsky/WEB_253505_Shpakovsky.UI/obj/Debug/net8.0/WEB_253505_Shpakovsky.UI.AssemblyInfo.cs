75f1cff370734f585590a9f066c925bf419558e8
anterg0 <antonxll50@gmail.com>
2024-09-19 19:29:35 +0300

    now lab1 is done

--- C# PROGRAMMING/sem5/WEB_253505_Shpakovsky/WEB_253505_Shpakovsky.UI/obj/Debug/net8.0/WEB_253505_Shpakovsky.UI.AssemblyInfo.cs
+++ C# PROGRAMMING/sem5/WEB_253505_Shpakovsky/WEB_253505_Shpakovsky.UI/obj/Debug/net8.0/WEB_253505_Shpakovsky.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB_253505_Shpakovsky.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b189849138d15feacff19cc4c20221c65ba5a7fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d5ac7ee93e5087bf6b670f329500543845e6a8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB_253505_Shpakovsky.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB_253505_Shpakovsky.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
