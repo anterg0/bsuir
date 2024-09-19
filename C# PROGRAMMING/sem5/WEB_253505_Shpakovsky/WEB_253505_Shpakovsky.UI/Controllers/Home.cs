1d5ac7ee93e5087bf6b670f329500543845e6a8b
anterg0 <antonxll50@gmail.com>
2024-09-19 12:37:41 +0300

    C# lab1 is done

---
+++ C# PROGRAMMING/sem5/WEB_253505_Shpakovsky/WEB_253505_Shpakovsky.UI/Controllers/Home.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_253505_Shpakovsky.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
